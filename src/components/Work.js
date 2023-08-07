1d84bd8cf66751cc40fa73f66822c7e9a925763b
antpica <antpica24@yahoo.com>
2023-08-07 18:03:20 -0400

    added project 3

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,8 +10,8 @@ const Work = () => {
             <div class="col-lg-6 mt-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Project 1</h4>
-                        <p class="card-text">BrewCast</p>
+                        <h4 class="card-title">BrewCast</h4>
+                        <p class="card-text">Project 1</p>
                         <div class="text-center">
                             <a href="https://domconf.github.io/BrewCast" class="btn btn-success">Link</a>
                         </div>
@@ -22,10 +22,21 @@ const Work = () => {
             <div class="col-lg-6 mt-4">
                 <div class="card portfolioContent">
                     <div class="card-body">
-                        <h4 class="card-title">Project 2</h4>
-                        <p class="card-text">Real Dills Pro Shop</p>
+                        <h4 class="card-title">Real Dills Pro Shop</h4>
+                        <p class="card-text">Project 2</p>
                         <div class="text-center">
                             <a href="https://real-dills-pro-shop.herokuapp.com/" class="btn btn-success">Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mt-4">
+                <div class="card portfolioContent">
+                    <div class="card-body">
+                        <h4 class="card-title">Cinema Stash</h4>
+                        <p class="card-text">Project 3</p>
+                        <div class="text-center">
+                            <a href="https://cinema-stash-c85de8364f90.herokuapp.com/" class="btn btn-success">Link</a>
                         </div>
                     </div>
                 </div>
